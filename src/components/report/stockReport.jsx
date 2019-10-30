fc7c13e372cc033ef9572d1d7066724a6e65edbb
salahuldin <salah.din@devsinc.com>
2019-10-30 15:35:26 +0500

    stock report table updated

--- src/components/report/stockReport.jsx
+++ src/components/report/stockReport.jsx
@@ -10,7 +10,7 @@ import { Button, Table, Container, Header, Image, Grid, Input } from "semantic-u
 const initialPagination = {
   activePage: 1,
   totalPages: 0,
-  per_page: 6
+  per_page: 10
 };
 
 class StockReport extends Component {
@@ -24,48 +24,48 @@ class StockReport extends Component {
       item: ""
     };
   }
-  
-  exportPDF = () => {
-      const unit = "pt";
-      const size = "A4"; // Use A1, A2, A3 or A4
-      const orientation = "portrait"; // portrait or landscape
-  
-      const marginLeft = 40;
-      const doc = new jsPDF(orientation, unit, size);
-  
-      doc.setFontSize(15);
-  
-      const title = "Items Stock Report";
-      const headers = [["NAME","CATEGORY", "STOCK", "UNIT PRICE"]];
-      const data = this.state.allItems.map(elt=> [elt.name, elt.category.name, elt.current_stock, elt.sale_price]);
 
-  
-      let content = {
-        startY: 50,
-        head: headers,
-        body: data
-      };
-  
-      doc.text(title, marginLeft, 40);
-      doc.autoTable(content);
-      doc.save("stock_report.pdf")
+  exportPDF = () => {
+    const unit = "pt";
+    const size = "A4"; // Use A1, A2, A3 or A4
+    const orientation = "portrait"; // portrait or landscape
+
+    const marginLeft = 40;
+    const doc = new jsPDF(orientation, unit, size);
+
+    doc.setFontSize(15);
+
+    const title = "Items Stock Report";
+    const headers = [["NAME", "CATEGORY", "STOCK", "UNIT PRICE"]];
+    const data = this.state.allItems.map(elt => [elt.name, elt.category.name, elt.current_stock, elt.sale_price]);
+
+
+    let content = {
+      startY: 50,
+      head: headers,
+      body: data
+    };
+
+    doc.text(title, marginLeft, 40);
+    doc.autoTable(content);
+    doc.save("stock_report.pdf")
 
   }
 
   handlePagination = (page, per_page) => {
-    this.setState({activePage: page, per_page:per_page });
-    const {item} = this.state;
+    this.setState({ activePage: page, per_page: per_page });
+    const { item } = this.state;
 
     http
-      .get(`${apiUrl}/api/v1/items`,{params:{page, per_page, item}})
+      .get(`${apiUrl}/api/v1/items`, { params: { page, per_page, item } })
       .then(res => {
         this.setState({
-          itemsData:res.data[1],
+          itemsData: res.data[1],
           totalPages: res.data[0].total,
-          isLoading:false
+          isLoading: false
         });
       });
-      this.setState({ state: this.state });
+    this.setState({ state: this.state });
   };
 
   getItems = () => {
@@ -77,9 +77,9 @@ class StockReport extends Component {
   };
 
   searchHandler = e => {
-    this.setState({ item: e.target.value },()=>{
-      const {per_page} = this.state;
-      this.handlePagination(1,per_page);
+    this.setState({ item: e.target.value }, () => {
+      const { per_page } = this.state;
+      this.handlePagination(1, per_page);
     });
   };
 
@@ -90,7 +90,7 @@ class StockReport extends Component {
   }
 
   render() {
-    const { itemsData, activePage, per_page, totalPages,isLoading } = this.state;
+    const { itemsData, activePage, per_page, totalPages, isLoading } = this.state;
 
     return (
       <div>
@@ -104,48 +104,52 @@ class StockReport extends Component {
           </Header>
         </Container>
         <div className="ui divider"></div>
-        <Grid columns={3}>
+        <Grid container="true">
           <Grid.Row>
-            <Grid.Column>
-                <Input
-                  icon="search"
-                  placeholder="Search Items"
-                  onChange={this.searchHandler}
-                />
+            <Grid.Column width="13" floated="left">
+              <Input
+                icon="search"
+                placeholder="Search Items"
+                onChange={this.searchHandler}
+              />
             </Grid.Column>
-            <Grid.Column floated="right">
+            <Grid.Column width="3">
               <Button
-              icon="download"
-              content="Download"
-              color="green"
-              onClick={() => this.exportPDF()}
-              style={{float:"right"}}
-            />
+                icon="download"
+                content="Download"
+                color="green"
+                onClick={() => this.exportPDF()}
+                floated="right"
+              />
             </Grid.Column>
           </Grid.Row>
-
         </Grid>
-        <Table celled>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Name</Table.HeaderCell>
-              <Table.HeaderCell>category</Table.HeaderCell>
-              <Table.HeaderCell>Current Stock</Table.HeaderCell>
-              <Table.HeaderCell>Unit Price</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-
-          <Table.Body>
-            {!isLoading? itemsData.map(item => (
-              <Table.Row key={item.id}>
-                <Table.Cell>{item.name}</Table.Cell>
-                <Table.Cell>{item.category.name}</Table.Cell>
-                <Table.Cell>{item.current_stock}</Table.Cell>
-                <Table.Cell>{item.sale_price}</Table.Cell>
-              </Table.Row>
-            )):<Loader/>}
-          </Table.Body>
-        </Table>
+        <div className="table-wrapper-scroll-y my-custom-scrollbar">
+          <table className="table table-bordered table-striped mb-1 mt-2 account-table">
+            <thead style={{ color: "white", background: "#1969a4" }}>
+              <tr>
+                <th scope="col">Item Code  </th>
+                <th scope="col">Item Name  </th>
+                <th scope="col">Category</th>
+                <th scope="col">Current Stock</th>
+                <th scope="col">Unit Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {!isLoading ? itemsData.map(item => {
+                return (
+                  <tr key={item.id}>
+                    <th scope="row">{item.code}</th>
+                    <td>{item.name}</td>
+                    <td>{item.category.name}</td>
+                    <td>{item.current_stock}</td>
+                    <td>{item.sale_price}</td>
+                  </tr>
+                )
+              }) : <Loader />}
+            </tbody>
+          </table>
+        </div>
         {totalPages > 0 ? (
           <Paginate
             handlePagination={this.handlePagination}
